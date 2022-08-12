e8bf24eebed55fd8df5f58b1172922f5b585922a
adelaiderobinson <adelaide.robinson445@gmail.com>
2022-08-12 16:39:25 -0700

    end of day

--- src/storm_runoff.R
+++ src/storm_runoff.R
@@ -1,3 +1,12 @@
-predict_runoff <- function(V,IA){
-   V*A
+#' Title
+#'
+#' @param A watershed area 
+#' @param IA fraction of the watershed considered impervious
+#'
+#' @return
+#' @export
+#'
+#' @examples
+predict_runoff <- function(A,IA){
+   A*(IA * 0.9 +0.05)
 }
